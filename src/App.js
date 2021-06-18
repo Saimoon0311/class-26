573544021085c1dd1f472c2138fd00e29c71274e
unknown <you@example.com>
2021-06-18 19:15:03 +0500

    class 26

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+// class App extends React.Component{
+//    render(){
+//      return(
+//        <div>
+//          <h1>
+//            hello world
+//          </h1>
+//        </div>
+//      )
+//    }
+// }
+
+// function App{
+//   return(
+//     <div>
+//       <h1>
+//         hello world
+//       </h1>
+//     </div>
+//   )  
+// }
+
+
+
+// class App extends React.Component{
+//    render(){
+//      let name = "saimoon"
+//      return(
+//        <div>
+//          <h1>
+//             {name}
+//          </h1>
+//        </div>
+//      )
+//    }
+// }
+
+
+class App extends React.Component{
+  render(){
+    let todo = [{text : ""}]
+    return(
+      <div>
+        <h1>
+           {name}
+        </h1>
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+export default App;
